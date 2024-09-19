3f2212d921b0c16036cf3dc41ae327dc299e7c00
도훈 <149929673+coehgns@users.noreply.github.com>
2024-09-19 20:18:22 +0900

    Delete src/Function.kt

--- src/Function.kt
+++
@@ -1,9 +0,0 @@
-fun main() {
-    var a: Int = 10000
-    var b: String = a.toString()
-    var c: Float = a.toFloat()
-    var d: Double = a.toDouble()
-    println(b)
-    println(c)
-    println(d)
-}

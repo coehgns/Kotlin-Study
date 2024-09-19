a72f93bdada9fe0365bb8b2f8d7547bdfa856c3a
도훈 <modooboiroo@gmail.com>
2024-09-19 20:16:48 +0900

    형변환

---
+++ src/Function.kt
@@ -0,0 +1,9 @@
+fun main() {
+    var a: Int = 10000
+    var b: String = a.toString()
+    var c: Float = a.toFloat()
+    var d: Double = a.toDouble()
+    println(b)
+    println(c)
+    println(d)
+}

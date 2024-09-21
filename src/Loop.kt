1fd9aacd444cd08b9b891448418d6660666bd963
도훈 <modooboiroo@gmail.com>
2024-09-21 14:37:32 +0900

    반복문

---
+++ src/Loop.kt
@@ -0,0 +1,45 @@
+fun main() {
+    // while
+    var i: Int = 0
+    while (i < 10) {
+        print("$i ")
+        i++
+    }
+    println()
+
+    // for
+    for (j: Int in 0 .. 5) {
+        print("$j ")
+    }
+
+    println()
+
+    val len: Int = 10
+    for (j in 0 .. len) {
+        print("$j ")
+    }
+
+    println()
+
+    for (j in 0 until len) {
+        print("$j ")
+    }
+
+    println()
+
+    for (j: Int in 0 .. 10 step(2)) {
+        print("$j ")
+    }
+
+    println()
+
+    for(i in 10 downTo 1) {
+        print("$i ")   // 출력 10 9 8 7 ... 1
+    }
+
+    println()
+
+    for(i in 10 downTo  1 step(2)) {
+        print("$i ")   // 출력 10 8 6 4 2
+    }
+}

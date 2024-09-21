6a0141c43c2a23ab835cae3e1ae36d15f6cacdfe
도훈 <modooboiroo@gmail.com>
2024-09-21 13:52:20 +0900

    조건문

---
+++ src/ConditionalStatement.kt
@@ -0,0 +1,13 @@
+fun main() {
+    val a: Int = 7
+    if(a > 10) {
+        println("10보다 큽니다.")
+    } else if(a < 10) {
+        println("10보다 작습니다.")
+    } else {
+        println("같습니다.")
+    }
+
+    val b: Int = 7
+    if (b>10) println("10보다 큽니다") else if(b < 10) println("10도 작습니다.") else println("같습니다.")
+}

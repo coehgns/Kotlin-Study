a5b046b4d5e2e50e3b45b380d5aa8897f8946497
도훈 <modooboiroo@gmail.com>
2024-09-21 13:55:38 +0900

    is를 사용한 데이터타입 비교

--- src/ConditionalStatement.kt
+++ src/ConditionalStatement.kt
@@ -10,4 +10,12 @@ fun main() {
 
     val b: Int = 7
     if (b>10) println("10보다 큽니다") else if(b < 10) println("10도 작습니다.") else println("같습니다.")
+
+    // is를 통한 데이터타입 비교
+    val thing: String = "hello world"
+    if(thing is String) {
+        println("String")
+    } else {
+        println("not String")
+    }
 }

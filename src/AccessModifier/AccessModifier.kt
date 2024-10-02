4ea274d100d53dedf2b5751bd3b9355c0e3ce543
도훈 <modooboiroo@gmail.com>
2024-10-02 21:15:38 +0900

    접근 제어자

--- src/AccessModifier/AccessModifier.kt
+++ src/AccessModifier/AccessModifier.kt
@@ -3,5 +3,5 @@ package AccessModifier
 class AccessModifier {
     private fun run() { } // AccessModifier에서만 접근 가능.
     public fun jump() { } // 모든 파일에서 접근 가능.(아무것도 쓰지 않을 경우 public.)
-    protected fun sit() { } // 클래스 자신과 상속받은 클래스 접근 가능.
+    protected fun sit() { } // 클래스 자신과 상속받은 클래스 접근 가능.e
 }
